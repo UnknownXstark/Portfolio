af3d742648f1c08d7f06a403c47dd245f19d13fc
UnknownXstark <nathanlouiskalio@gmail.com>
2025-08-30 13:44:03 +0100

    updates to about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About/>
     </>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,17 +2,55 @@ import React from "react";
 import "./About.css";
 
 const About = () => {
-  return <div className="about">
-    <div className="about-title">
+  return (
+    <div className="about">
+      <div className="about-title">
         <h1>About Me</h1>
         <img src="" alt="" />
-    </div>
-    <div className="about-section">
+      </div>
+      <div className="about-section">
         <div className="about-left">
-            <img src="" alt="" />
+          <img src="" alt="" />
         </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p></p>
+            <p></p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "80%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JavaScript</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>React JS</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+            <div className="about-skill">
+              <p>Python</p>
+              <hr style={{ width: "40%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievments">
+        <div className="about-achievement">
+          <h1>1</h1>
+          <p>YEAR OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>2</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default About;
