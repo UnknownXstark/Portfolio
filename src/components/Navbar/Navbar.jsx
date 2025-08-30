f0f8598c1931693099acf4280494e01fbc61717b
UnknownXstark <nathanlouiskalio@gmail.com>
2025-08-30 11:40:29 +0100

    updates

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,15 @@ import './Navbar.css'
 const Navbar = () => {
   return (
     <div className='navbar'>
-        
+        <img src="" alt="" />
+        <ul className='nav-menu'>
+            <li>Home</li>
+            <li>About Me</li>
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>Contact</li>
+        </ul>
+        <div className='nav-connect'>Connect With Me</div>
     </div>
   )
 }
