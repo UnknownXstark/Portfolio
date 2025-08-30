140f7d504a243a8381dd398f7973aedb8cefee78
UnknownXstark <nathanlouiskalio@gmail.com>
2025-08-30 12:02:27 +0100

    updates

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const hero = () => {
   return (
     <div className='hero'>
         <img src="" alt="" />
-        <h1>My Name is Nathan Kalio, a frontend developer based in Nigeria.</h1>
+        <h1><span>I'm Nathan Kalio, </span>a frontend developer based in Nigeria.</h1>
         <p>I am a frontend developer from Port Harcourt, Nigeria with 1 year of experience</p>
         <div className='hero-action'>
             <div className="hero-connect">Connect with me</div>
