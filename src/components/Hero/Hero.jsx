2da8f4b866be527d7c646c304d8cdf57a2040305
UnknownXstark <nathanlouiskalio@gmail.com>
2025-08-30 11:48:23 +0100

    updates

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hero.css'
+
+const hero = () => {
+  return (
+    <div className='hero'>
+        <img src="" alt="" />
+    </div>
+  )
+}
+
+export default hero
